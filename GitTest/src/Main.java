6d3f8d0eb8d294795d41c9aef4192c2bc85d9fdd
DESKTOP-3BREUIH\xaiop <xaiop1324@naver.com>
2017-01-19 11:23:22 +0900

    no message

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 	
-	public static void main(String[] ar) {
+	public static void main(String[] args) {
 		System.out.println("æ»≥Á«œººø‰!");
+		System.out.println("π›∞©Ω¿¥œ¥Ÿ.");
 	}
 }
