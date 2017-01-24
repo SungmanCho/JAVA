0f584ef971b0ceb6e4edc73ebdd931b47a7c5eff
DESKTOP-3BREUIH\xaiop <xaiop1324@naver.com>
2017-01-24 11:44:24 +0900

    2017년 1월 24일 화요일 강의 내용

--- OOP/src/com/ktds/sungman/Student.java
+++ OOP/src/com/ktds/sungman/Student.java
@@ -1,14 +1,43 @@
 package com.ktds.sungman;
 
 public class Student {
-		private String s = "공부";
+		private String s = "공부";//외부에서 무분별한 수정을 막기 위해서 쓴다.
 		private String st = "학생";
 		private String f = "급식";
 		private String b = "교과서";
 		private String n = "조성만";
 		private String t = "시험";
-		
+	
+		//instance field에 대해서 setter를 생성
+		//setter의 역할은 데이터를 갱신 시켜주는 것이다. 데이터를 함부로 바꿀수 없게 하는 것.
+		/*public void setName() {
+			// name  인스턴스 필드를 갱신함.
+		}*/
+		/*public void set멤버변수명(멤버변수타입 멤버변수명){
+			this.set멤버변수명 = 멤버변수명;
+		}*/
+		public void setStudy(String study){
+			if(study.length()==5){
+			this.s = study;
+			}
+		}
+		public void setName(String name){
+			if(name.length()==2){
+			this.n = name;
+			}
+		}
+		public void setEat(String eat){
+			if(eat.length()==4){
+			this.f = eat;
+			}
+		}
+		public void setTest(String test){
+			if(test.length()==3){
+			this.t = test;
+			}
+		}
 		public void study(){
+			
 			String message= String.format("%s를 하기 위해 %s를 폅니다.",s,b);
 			System.out.println(message);
 		}
--- OOP/src/com/ktds/sungman/StudentMain.java
+++ OOP/src/com/ktds/sungman/StudentMain.java
@@ -2,13 +2,26 @@ package com.ktds.sungman;
 
 public class StudentMain {
 	public static void  main(String[] args){
-	Student student = new Student();
+	Student csm = new Student();//메소드
 	
-	student.study();
-	student.name();
+	csm.study();
+	csm.name();
+	//csm.setName("조성만");
+	//csm.name = "조성만";
+	csm.eat();
+	csm.test();
 	
-	student.eat();
-	student.test();
+	csm.setStudy("study");
+	csm.setName("원빈");
+	csm.setEat("맥도날드");
+	csm.setTest("오디션");
+	
+	csm.study();
+	csm.name();
+	//csm.setName("조성만");//메소드이다.
+	//csm.name = "조성만";
+	csm.eat();
+	csm.test();
 	}
 }
 
