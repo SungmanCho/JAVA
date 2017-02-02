b61a2c463ca97bdf5cdf1da84c87c88f2d83ad18
DESKTOP-3BREUIH\xaiop <xaiop1324@naver.com>
2017-02-02 16:59:06 +0900

    2월 2일 목요일 예외처리

--- EXCEPTION/src/com/ktds/sungman/EXCEPTION.java
+++ EXCEPTION/src/com/ktds/sungman/EXCEPTION.java
@@ -1,10 +1,21 @@
 package com.ktds.sungman;
 
-import java.util.Scanner;
-
 public class EXCEPTION {
-public static void main(String[] args) {
-	Scanner scanner = null;
-	String text = scanner.nextLine();
-}
+	public static void main(String[] args) {
+		try {
+			convertStringToNumber(null);
+		} catch (Exception e) {
+			
+			System.out.println("문자를 다시 생성합니다.");
+			System.out.println(e.getMessage());
+
+		}
+	}
+
+	public static void convertStringToNumber(String str) throws Exception {
+		if (str == null) {
+			throw new Exception("파라미터를 작성해 주세요!!!!!");// 트라이 캐치 책임을 전가하는 것이다.
+		}
+		int convertedNumber = Integer.parseInt("NUM");
+	}
 }
---
+++ EXCEPTION/src/com/ktds/sungman/Student.java
@@ -0,0 +1,44 @@
+package com.ktds.sungman;
+
+import com.ktds.sungman.exceptions.MissMatchValueException;
+import com.ktds.sungman.exceptions.MissingValueException;
+
+public class Student {
+	public void start() throws MissingValueException {
+		try {
+			registNewStudent();
+		} finally{
+			System.out.println("잘 처리 됐습니다.");
+		}
+		System.out.println("정상적으로 수행되었습니다.");
+	}
+
+	public void registNewStudent() throws MissingValueException {
+		{
+			String name = "";// 아무것도 없는 것
+			String school = "ktds1";
+
+			if (name == null) {
+				throw new MissingValueException("이름은 필수 입력 값입니다.");// Exception이다.
+																	// throws
+																	// 되자마자 밑에건
+																	// 신경도 안쓴다.
+																	// 자기가 던진거라서
+																	// throws를
+																	// 건다.
+			}
+			if (!school.equals("ktds")) {
+				throw new MissMatchValueException("학교이름이 잘못 되었습니다.");// 런타임이라서
+																		// 빨간줄이
+																		// 나오지
+																		// 않는다.
+			}
+		}
+	}
+
+	public static void main(String[] args) throws MissingValueException, MissMatchValueException {
+
+		new Student().start();
+	}
+
+}
