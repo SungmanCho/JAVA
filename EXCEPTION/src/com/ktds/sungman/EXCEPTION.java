74f52f73e450e3fa86f82e4f824afd03326f9cf3
DESKTOP-3BREUIH\xaiop <xaiop1324@naver.com>
2017-02-02 13:58:03 +0900

    2월 2일 예외처리

---
+++ EXCEPTION/src/com/ktds/sungman/EXCEPTION.java
@@ -0,0 +1,10 @@
+package com.ktds.sungman;
+
+import java.util.Scanner;
+
+public class EXCEPTION {
+public static void main(String[] args) {
+	Scanner scanner = null;
+	String text = scanner.nextLine();
+}
+}
